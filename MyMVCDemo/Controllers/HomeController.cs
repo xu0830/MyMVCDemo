90e38042ad600e9168ad0b3c9519a97e7d518502
xucanjie <xucanjie1071@163.com>
2019-03-10 22:58:27 +0800

    set how to get passenger dto

--- MyMVCDemo/Controllers/HomeController.cs
+++ MyMVCDemo/Controllers/HomeController.cs
@@ -42,6 +42,10 @@ namespace MyMVCDemo.Controllers
             return View();
         }
 
+        /// <summary>
+        /// 获取验证码图片
+        /// </summary>
+        /// <returns></returns>
         public ActionResult GetImage()
         {
             var client = new RestClient("https://kyfw.12306.cn/passport/captcha/captcha-image64");
@@ -67,7 +71,6 @@ namespace MyMVCDemo.Controllers
                 cookie = WebConst.cookieContainer
             });
         }
-        
 
         public ActionResult CheckPoint(List<int> points)
         {
@@ -83,12 +86,12 @@ namespace MyMVCDemo.Controllers
 
             WebConst.Points = answer;
 
+            #region 验证码校验
             var client = new RestClient("https://kyfw.12306.cn/passport/captcha/captcha-check?answer=" + answer + "&rand=sjran&login_site=E");
             var request = new RestRequest(Method.POST);
             request.AddHeader("Postman-Token", "2ff56327-d255-43da-8712-81a4dcdb5882");
             request.AddHeader("cache-control", "no-cache");
             request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
-
 
             if (WebConst.cookieContainer.Count > 0)
             {
@@ -97,8 +100,10 @@ namespace MyMVCDemo.Controllers
                     request.AddParameter(cookie.Name, cookie.Value, ParameterType.Cookie);
                 }
             }
-
             IRestResponse response = client.Execute(request);
+            #endregion
+
+            #region 登录接口
             var loginAnswer = answer.Replace(",", "%2C");
             var client_2 = new RestClient("https://kyfw.12306.cn/passport/web/login");
             var request_2 = new RestRequest(Method.POST);
@@ -108,7 +113,7 @@ namespace MyMVCDemo.Controllers
             //  request_2.AddHeader("referer", "https://kyfw.12306.cn/otn/resources/login.html");
             request_2.AddHeader("Content-Type", "application/x-www-form-urlencoded");
             //  request_2.AddHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36");
-            request_2.AddParameter("application/x-www-form-urlencoded", "username=13428108149&password=xucanjie88&answer="+ loginAnswer + "&appid=otn&undefined=", ParameterType.RequestBody);
+            request_2.AddParameter("application/x-www-form-urlencoded", "username=13428108149&password=xucanjie88&answer=" + loginAnswer + "&appid=otn&undefined=", ParameterType.RequestBody);
 
             if (WebConst.cookieContainer.Count > 0)
             {
@@ -119,26 +124,10 @@ namespace MyMVCDemo.Controllers
             }
 
             IRestResponse response_2 = client_2.Execute(request_2);
+            #endregion
 
-
-			/*
-			cookie
-				JSESSIONID=D77475C9D9DAF9C4E8E608946F3382D3;
-				tk=aCG3oDfoFGXjQwkUZavSkd62S20Myi-8s4cdUZyI_yUlm1210;
-				_jc_save_wfdc_flag=dc; 
-				_jc_save_fromStation=%u6DF1%u5733%u5317%2CIOQ;
-				_jc_save_toStation=%u666E%u5B81%2CPEQ; 
-				RAIL_EXPIRATION=1552149503783;
-				RAIL_DEVICEID=KZq3-zlAIAndGmzfOvghxAoGVjNH1WU3W4_v5b8SK_e2iB3yoVEj1hVGgECc7kovHnA6g4lly7OJJUrUtKWD6ZcNtX5XDGWG_As-doG05v845_tQZtT31_9edWErGowQhY-yEiv0OHg0RkONwIeM7tkeCmoisodf; 
-				_jc_save_toDate=2019-03-06; 
-				_jc_save_fromDate=2019-04-01;
-				route=495c805987d0f5c8c84b14f60212447d;
-				BIGipServerotn=720372234.64545.0000;
-				BIGipServerpool_passport=401408522.50215.0000
-				
-				If-Modified-Since: 0
-			*/
-			var client_3 = new RestClient("https://kyfw.12306.cn/passport/web/auth/uamtk");
+            #region 获取标识登录状态的cookie
+            var client_3 = new RestClient("https://kyfw.12306.cn/passport/web/auth/uamtk");
             var request_3 = new RestRequest(Method.POST);
             request_3.AddHeader("cache-control", "no-cache");
             request_3.AddParameter("application/x-www-form-urlencoded", "appid=otn", ParameterType.RequestBody);
@@ -154,16 +143,16 @@ namespace MyMVCDemo.Controllers
             IRestResponse response_3 = client_3.Execute(request_3);
 
             ResponseModel responseModel = JsonConvert.DeserializeObject<ResponseModel>(response_3.Content);
-
-
+   
             var client_4 = new RestClient("https://kyfw.12306.cn/otn/uamauthclient");
             var request_4 = new RestRequest(Method.POST);
             request_4.AddHeader("cache-control", "no-cache");
-            request_4.AddParameter("application/x-www-form-urlencoded", "tk="+ responseModel.Newapptk, ParameterType.RequestBody);
+            request_4.AddParameter("application/x-www-form-urlencoded", "tk=" + responseModel.Newapptk, ParameterType.RequestBody);
 
             IRestResponse response_4 = client_4.Execute(request_4);
+            #endregion
 
-
+            #region 校验登陆状态
             var client_5 = new RestClient("https://kyfw.12306.cn/otn/login/checkUser");
             var request_5 = new RestRequest(Method.POST);
             request_5.AddHeader("cache-control", "no-cache");
@@ -175,9 +164,10 @@ namespace MyMVCDemo.Controllers
                     request_5.AddParameter(item.Name, item.Value, ParameterType.Cookie);
                 }
             }
-
             IRestResponse response_5 = client_5.Execute(request_5);
+            #endregion
 
+            #region 获取乘客信息
             var client_6 = new RestClient("https://kyfw.12306.cn/otn/confirmPassenger/getPassengerDTOs");
             var request_6 = new RestRequest(Method.POST);
             request_6.AddHeader("cache-control", "no-cache");
@@ -189,8 +179,10 @@ namespace MyMVCDemo.Controllers
                     request_6.AddParameter(item.Name, item.Value, ParameterType.Cookie);
                 }
             }
-
             IRestResponse response_6 = client_6.Execute(request_6);
+
+            #endregion
+
 
 
             return Json(new
